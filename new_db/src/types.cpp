3828c173a6959d690e32ee60d04984cdee9d2c2f
Arnav-Purushotam-CUBoulder <arpu9852@colorado.edu>
2025-08-17 17:56:56 -0600

    Core: implement value types

---
+++ new_db/src/types.cpp
@@ -0,0 +1,135 @@
+#include "types.h"
+#include "extensions/highwayhash.h"
+
+#include <algorithm>
+#include <type_traits>
+#include <typeinfo>
+
+namespace new_db {
+
+HashedValue::HashedValue(const Bytes& data,std::size_t length,std::size_t hash)
+  : m_has_value(true),
+    m_data(data),
+    m_length(length),
+    m_hash(hash)
+{
+  ;
+}
+
+HashedValue::HashedValue()
+  : m_has_value(false),
+    m_data(),
+    m_length(0),
+    m_hash(0)
+{
+  ;
+}
+
+/** Copy/move constuctors and operators **/
+HashedValue::HashedValue(const HashedValue& from)
+  : m_has_value(from.m_has_value),
+    m_data(from.m_data),
+    m_length(from.m_length),
+    m_hash(from.m_hash)
+{
+  ;
+}
+
+HashedValue::HashedValue(const HashedValue&& from)
+  : m_has_value(from.m_has_value),
+    m_data(std::move(from.m_data)),
+    m_length(from.m_length),
+    m_hash(from.m_hash)
+{
+  //std::cout << "HashedValue::move-ctor" << std::endl;
+  ;
+}
+
+HashedValue&
+HashedValue::operator=(const HashedValue& other)
+{
+  m_has_value = other.m_has_value;
+  m_length = other.m_length;
+  m_data = other.m_data;
+  m_hash = other.m_hash;
+  return *this;
+}
+
+/** Conversion constuctors **/
+HashedValue::HashedValue(const EncodedValue& from)
+  : m_has_value(from.hasValue()),
+    m_data(from.data()),
+    m_length(from.length()),
+    m_hash(from.hash())
+{
+  ;
+}
+
+HashedValue::HashedValue(EncodedValue&& from)
+  : m_has_value(from.hasValue()),
+    m_data(std::move(from.data())),
+    m_length(from.length()),
+    m_hash(from.hash())
+{
+  ;
+}
+
+const Bytes
+HashedValue::data() const { return m_data; }
+
+std::size_t
+HashedValue::length() const { return m_length; }
+
+std::size_t
+HashedValue::hash() const { return m_hash; }
+
+bool
+HashedValue::hasValue() const { return m_has_value; }
+
+bool
+HashedValue::operator==(const HashedValue& other) const
+{
+  if (m_hash != other.m_hash) {
+    return false;
+  }
+  // compare hash first as generally it will be faster most often
+  if (m_length != other.m_length) {
+    return false;
+  }
+  // only do a data wise comparison if you must (highly, highly unlikely - requires hash collision)
+  //return 0 == std::strcmp(m_data,other.m_data);
+  return m_data == other.m_data;
+}
+
+bool
+HashedValue::operator!=(const HashedValue& other) const
+{
+  return !(*this==other);
+}
+
+bool
+EncodedValue::operator==(const EncodedValue& other) const
+{
+  return m_has_value==other.m_has_value && m_type == other.m_type && (m_value == other.m_value);
+}
+
+bool
+EncodedValue::operator!=(const EncodedValue& other) const
+{
+  return !(*this==other);
+}
+
+std::ostream &
+operator<<(std::ostream &os, const Type t) {
+  os << (int)t;
+  return os;
+}
+
+std::istream &operator>>(std::istream &is, Type t) {
+  int tint;
+  is >> tint;
+  t = (Type)tint;
+  return is;
+}
+
+}
