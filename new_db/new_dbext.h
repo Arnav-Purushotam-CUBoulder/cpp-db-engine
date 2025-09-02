d61bcfdc11df8feb2eed15745b9284c1c5183d68
Arnav-Purushotam-CUBoulder <arpu9852@colorado.edu>
2025-09-02 02:20:20 -0600

    Legacy: add legacy new_dbext.h

---
+++ new_db/new_dbext.h
@@ -0,0 +1,6 @@
+// WARNING DO NOT EDIT - EDIT include/new_db.h instead!
+// (This file is only used by tests and CLI, NOT included in the client API distro!)
+#include "new_db.h"
+#include "include/extensions/highwayhash.h"
+#include "include/extensions/extquery.h"
+#include "include/extensions/extdatabase.h"
