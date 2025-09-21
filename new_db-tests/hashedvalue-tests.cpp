db923c7ce99b6e495e44c337247e6dffae0bc327
Arnav-Purushotam-CUBoulder <arpu9852@colorado.edu>
2025-09-21 12:57:57 -0600

    Tests: add hashed value tests

---
+++ new_db-tests/hashedvalue-tests.cpp
@@ -0,0 +1,50 @@
+#include "catch.hpp"
+
+#include "new_db/new_db.h"
+
+TEST_CASE("hashedvalue","[types][hashedvalue]") {
+
+  SECTION("string") {
+    std::string str("some unique and amazing string");
+    std::string str2("some unique and amazing string");
+    new_db::HashedValue ev1(str);
+    new_db::HashedValue ev2(str2);
+
+    REQUIRE(0 != str.length()); // ensure it hasn't had its contents moved
+    REQUIRE(0 != str2.length()); // ensure it hasn't had its contents moved
+    REQUIRE(str.length() == str2.length()); // idiot check
+    REQUIRE(ev1.hasValue());
+    REQUIRE(ev2.hasValue());
+    REQUIRE(str.length() == ev1.length());
+    REQUIRE(str.length() == ev2.length());
+    char ev1chars[ev1.length()];
+    int pos = 0;
+    for (auto& c : ev1.data()) {
+      ev1chars[pos++] = (const char)c;
+    }
+    char ev2chars[ev2.length()];
+    pos = 0;
+    for (auto& c : ev2.data()) {
+      ev2chars[pos++] = (const char)c;
+    }
+    //REQUIRE(0 == strcmp(str.c_str(),ev1.data()));
+    //std::cout << "ev1 data: '" << ev1.data() << "', ev2 data: '" << ev2.data() << "'" << std::endl;
+    REQUIRE(0 == strcmp(ev1chars,ev2chars));
+    REQUIRE(0 != ev1.hash());
+    REQUIRE(0 != ev2.hash());
+    REQUIRE(ev1.hash() == ev2.hash());
+
+    // elements of == function
+    REQUIRE(ev1.hash()==ev2.hash());
+    REQUIRE(ev1.hasValue()==ev2.hasValue());
+    REQUIRE(ev1.length()==ev2.length());
+    REQUIRE(0==strcmp(ev1chars,ev2chars));
+    // actual == function
+    REQUIRE(ev1 == ev2);
+
+    REQUIRE(8 == sizeof(ev1.hash()));
+    REQUIRE(8 == sizeof(ev2.hash()));
+  }
+
+
+}
